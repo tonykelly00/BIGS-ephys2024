6a88fe26ae1ece6bd8e3a68120edd0a1c6221645
Tony Kelly <tony.kelly00@gmail.com>
2024-08-28 20:48:01 +0200

    BIGS notebook for colab

--- src/generate_select_markers.py
+++ src/generate_select_markers.py
@@ -1,10 +1,10 @@
 
 import pandas as pd
 import numpy as np
+import scanpy as sc
 
 def generate_SelectMarkers(gene_data, metadata):
-    import scanpy as sc
-
+    
     # Assuming gene_data is your DataFrame with genes as rows and cells as columns
     adata = sc.AnnData(gene_data.T)
 
@@ -30,8 +30,23 @@ def generate_SelectMarkers(gene_data, metadata):
     marker_genes = pd.DataFrame(adata.uns['rank_genes_groups']['names']).head(50)
 
     # # Save to CSV
-    marker_genes.to_csv('downloads/select_markers.csv', index=False)
+    marker_genes.to_csv('../downloads/select_markers.csv', index=False)
 
 
-def generate_SelectMarkers(gene_data, metadata):
-    import scanpy as sc
+def generate_gene_umap(gene_data):
+    #method1
+    # Read the CSV file
+    marker_genes_for_umap = pd.read_csv("../downloads/select_markers.csv")
+
+    # Melt the DataFrame to have a single column of gene names
+    marker_genes_for_umap = marker_genes_for_umap.melt(value_name="Gene").dropna()["Gene"]
+
+    # Ensure the gene names are unique
+    marker_genes_for_umap = marker_genes_for_umap.drop_duplicates()
+
+    # Check if the gene names exist in gene_data
+    missing_genes = marker_genes_for_umap[~marker_genes_for_umap.isin(gene_data.index)]
+    if not missing_genes.empty:
+        print(f"Warning: The following genes are missing in gene_data: {missing_genes.tolist()}")
+
+    return marker_genes_for_umap
